be87c82f65886720db749195db499fb5a61a6341
Paulina Żurawska <zurawskapa@gmail.com>
2018-12-23 14:31:48 +0100

    first project

---
+++ ProjectVersion01/ProjectVersion01/Controllers/BicyclesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EFGetStarted.AspNetCore.NewDb.Models;
+
+namespace ProjectVersion01.Controllers
+{
+    public class BicyclesController : Controller
+    {
+        private readonly ShopContext _context;
+
+        public BicyclesController(ShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bicycles
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Bicycles.ToListAsync());
+        }
+
+        // GET: Bicycles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bicycle = await _context.Bicycles
+                .FirstOrDefaultAsync(m => m.BicycleId == id);
+            if (bicycle == null)
+            {
+                return NotFound();
+            }
+
+            return View(bicycle);
+        }
+
+        // GET: Bicycles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bicycles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BicycleId,Name,Description,Price")] Bicycle bicycle)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bicycle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bicycle);
+        }
+
+        // GET: Bicycles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bicycle = await _context.Bicycles.FindAsync(id);
+            if (bicycle == null)
+            {
+                return NotFound();
+            }
+            return View(bicycle);
+        }
+
+        // POST: Bicycles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BicycleId,Name,Description,Price")] Bicycle bicycle)
+        {
+            if (id != bicycle.BicycleId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bicycle);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BicycleExists(bicycle.BicycleId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bicycle);
+        }
+
+        // GET: Bicycles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bicycle = await _context.Bicycles
+                .FirstOrDefaultAsync(m => m.BicycleId == id);
+            if (bicycle == null)
+            {
+                return NotFound();
+            }
+
+            return View(bicycle);
+        }
+
+        // POST: Bicycles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bicycle = await _context.Bicycles.FindAsync(id);
+            _context.Bicycles.Remove(bicycle);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BicycleExists(int id)
+        {
+            return _context.Bicycles.Any(e => e.BicycleId == id);
+        }
+    }
+}
---
+++ ProjectVersion01/ProjectVersion01/Models/Model.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EFGetStarted.AspNetCore.NewDb.Models
+{
+
+
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options)
+            : base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Purchase>()
+                .HasKey(pr => new { pr.BicycleId, pr.PersonId });
+            modelBuilder.Entity<Purchase>()
+                .HasOne(pr => pr.Person)
+                .WithMany(p => p.Purchases)
+                .HasForeignKey(pr => pr.PersonId);
+            modelBuilder.Entity<Purchase>()
+                .HasOne(pr => pr.Bicycle)
+                .WithMany(b => b.Purchases)
+                .HasForeignKey(pr => pr.BicycleId);
+        }
+
+        public DbSet<Person> People { get; set; }
+        public DbSet<Bicycle> Bicycles { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+
+
+
+
+    }
+   
+
+    public class Purchase
+    {
+        public int PersonId { get; set; }
+        public Person Person { get; set; }
+        public int BicycleId { get; set; }
+        public Bicycle Bicycle { get; set; }
+
+    }
+
+    public class Bicycle
+    {
+        public int BicycleId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public ICollection<Purchase> Purchases { get; set; }
+
+
+    }
+
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Pasword { get; set;}
+        public string Role { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public ICollection<Purchase> Purchases { get; set; }
+
+    }
+
+
+
+   
+
+}
